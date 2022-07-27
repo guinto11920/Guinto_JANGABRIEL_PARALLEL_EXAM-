555de18a3dd9f3a404c1e95133dda3dbcfb86865
guinto11920 <90599352+guinto11920@users.noreply.github.com>
2022-07-28 03:56:16 +0800

    Create LEVEL2_divisible_by3,4,5.py
    Write a program that will display all numbers divisible by 3, 4 and 5
    from 1-50.

---
+++ LEVEL2_divisible_by3,4,5.py
@@ -0,0 +1,10 @@
+
+for num in range(51):
+        if num % 3 == 0 :
+            print( str(num) + " ", end="")
+        elif num % 4 == 0 :
+            print( str(num) + " ", end="")
+        elif num % 5 == 0 :
+            print( str(num) + " ", end="")
+        else:
+            pass
